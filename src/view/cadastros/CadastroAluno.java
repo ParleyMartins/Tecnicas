3bc72ff074507f84a0045c5ab8c5a242c06660f9
Parley <parleypachecomartins@gmail.com>
2014-03-24 16:14:47 -0300

    Classes CadastroProfessor.java and CadastroAluno.java modified to fit Style and Design rules

--- src/view/cadastros/CadastroAluno.java
+++ src/view/cadastros/CadastroAluno.java
@@ -1,44 +1,50 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ 	RegisterStudent 
+ 	This class allows the user to register a student
+ 	https://github.com/ParleyMartins/Tecnicas/tree/estiloDesign/src/view/cadastros
  */
 package view.cadastros;
 
 import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
-
 import control.ManterAluno;
 import exception.ClienteException;
 
-/**
- * 
- * @author Parley
- */
 public class CadastroAluno extends CadastroCliente {
 
-    public CadastroAluno(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-        this.setName("CadastroAluno");
-    }
+	// Constructor creates a RegisterStudent form.
+	public CadastroAluno (java.awt.Frame parent, boolean modal) {
 
-    @Override public void cadastroAction() {
-        try {
-            if (cadastroBtn.getText().equals("Cadastrar")) {
-                ManterAluno.getInstance().inserir(nomeTxtField.getText(), cpfTxtField.getText(), matriculaTxtField.getText(),
-                        telefoneTxtField.getText(), emailTxtField.getText());
+		super(parent, modal);
+		this.setName("CadastroAluno");
+	}
 
-                JOptionPane.showMessageDialog(this, "Aluno Cadastrado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE,
-                        null);
+	@Override
+	// This method registers a student
+	public void cadastroAction ( ) {
 
-                this.setVisible(false);
-            }
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (NullPointerException ex) {
-            JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        }
-    }
+		try {
+			if (this.cadastroBtn.getText().equals("Cadastrar")) {
+				ManterAluno.getInstance().inserir(this.nomeTxtField.getText(),
+						this.cpfTxtField.getText(), this.matriculaTxtField.getText(),
+						this.telefoneTxtField.getText(), this.emailTxtField.getText());
+
+				JOptionPane.showMessageDialog(this,
+						"Aluno Cadastrado com sucesso", "Sucesso",
+						JOptionPane.INFORMATION_MESSAGE,
+						null);
+
+				this.setVisible(false);
+			}
+		} catch (ClienteException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro",
+					JOptionPane.ERROR_MESSAGE, null);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro",
+					JOptionPane.ERROR_MESSAGE, null);
+		} catch (NullPointerException ex) {
+			JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(),
+					"Erro", JOptionPane.ERROR_MESSAGE, null);
+		}
+	}
 }
