1e54a23d1c7443f748db5bb5211286da69fa97d1
Parley <parleypachecomartins@gmail.com>
2014-03-24 15:40:01 -0300

    Classes AlterarProfessor.java and AlterarSala.java modified to fit the Style and Design rules. .form files removed

--- src/view/alteracoes/AlterarProfessor.java
+++ src/view/alteracoes/AlterarProfessor.java
@@ -1,58 +1,74 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ 	ModifyTeacher
+ 	This class allows user to modify one or more fields of teacher.
+ 	https://github.com/ParleyMartins/Tecnicas/tree/estiloDesign/src/view/alteracoes
  */
 package view.alteracoes;
 
 import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
-
 import view.cadastros.CadastroCliente;
 import control.ManterProfessor;
 import exception.ClienteException;
 
-/**
- * 
- * @author Parley
- */
 public class AlterarProfessor extends CadastroCliente {
 
-    int index2 = 0;
+	int index2 = 0;
 
-    public AlterarProfessor(java.awt.Frame parent, boolean modal, int index) {
-        super(parent, modal);
-        this.setName("AlterarProfessor");
-        this.cadastroBtn.setText("Alterar");
-        this.cadastroBtn.setName("Alterar");
-        this.index2 = index;
+	// Constructor creates a ModifyTeacher form.
+	public AlterarProfessor (java.awt.Frame parent, boolean modal, int index) {
 
-        try {
-            this.nomeTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getNome());
-            this.emailTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getEmail());
-            this.telefoneTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getTelefone());
-            this.matriculaTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getMatricula());
-            this.cpfTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getCpf());
+		super(parent, modal);
+		this.setName("AlterarProfessor");
+		this.cadastroBtn.setText("Alterar");
+		this.cadastroBtn.setName("Alterar");
+		this.index2 = index;
 
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        }
-    }
+		try {
+			this.nomeTxtField.setText(ManterProfessor.getInstance()
+					.getProfessores_vet().get(index).getNome());
+			this.emailTxtField.setText(ManterProfessor.getInstance()
+					.getProfessores_vet().get(index).getEmail());
+			this.telefoneTxtField.setText(ManterProfessor.getInstance()
+					.getProfessores_vet().get(index).getTelefone());
+			this.matriculaTxtField.setText(ManterProfessor.getInstance()
+					.getProfessores_vet().get(index).getMatricula());
+			this.cpfTxtField.setText(ManterProfessor.getInstance()
+					.getProfessores_vet().get(index).getCpf());
 
-    @Override public void cadastroAction() {
-        try {
-            ManterProfessor.getInstance().alterar(nomeTxtField.getText(), cpfTxtField.getText(), matriculaTxtField.getText(),
-                    telefoneTxtField.getText(), emailTxtField.getText(),
-                    ManterProfessor.getInstance().getProfessores_vet().get(index2));
+		} catch (ClienteException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro",
+					JOptionPane.ERROR_MESSAGE, null);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro",
+					JOptionPane.ERROR_MESSAGE, null);
+		}
+	}
 
-            JOptionPane.showMessageDialog(this, "Cadastro alterado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
-            this.setVisible(false);
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        }
-    }
+	@Override
+	// Creates an action to modify a teacher.
+	public void cadastroAction ( ) {
+
+		try {
+			ManterProfessor.getInstance().alterar(
+					this.nomeTxtField.getText(),
+					this.cpfTxtField.getText(),
+					this.matriculaTxtField.getText(),
+					this.telefoneTxtField.getText(),
+					this.emailTxtField.getText(),
+					ManterProfessor.getInstance().getProfessores_vet()
+							.get(this.index2));
+
+			JOptionPane.showMessageDialog(this,
+					"Cadastro alterado com sucesso", "Sucesso",
+					JOptionPane.INFORMATION_MESSAGE, null);
+			this.setVisible(false);
+		} catch (ClienteException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro",
+					JOptionPane.ERROR_MESSAGE, null);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro",
+					JOptionPane.ERROR_MESSAGE, null);
+		}
+	}
 }
