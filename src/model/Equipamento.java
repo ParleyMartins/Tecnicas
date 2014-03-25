f9505894f6658ea147e48c8f62dedcd844d65c3a
thiagotmb <thiagobernardes10@gmail.com>
2014-03-24 22:09:24 -0300

    Header of class modified to default design.

--- src/model/Equipamento.java
+++ src/model/Equipamento.java
@@ -1,3 +1,9 @@
+/**
+Equipamento
+Class sets exceptions of Equipamento.
+https://github.com/ParleyMartins/Tecnicas/tree/master/src/model/Equipamento.java.
+*/
+
 package model;
 
 import exception.PatrimonioException;
