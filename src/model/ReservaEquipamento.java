fc9426f75a93138dd395e28f82eb38b49580fd2e
thiagotmb <thiagobernardes10@gmail.com>
2014-03-25 18:34:32 -0300

    Eclipse formatter applied

--- src/model/ReservaEquipamento.java
+++ src/model/ReservaEquipamento.java
@@ -1,8 +1,8 @@
 /**
-		ReservaEquipamento.
-		Class sets exceptions of ReservaEquipamento.
-		https://github.com/ParleyMartins/Tecnicas/tree/master/src/model/ReservaEquipamento.java.
-*/
+ * ReservaEquipamento. Class sets exceptions of ReservaEquipamento.
+ * https://github
+ * .com/ParleyMartins/Tecnicas/tree/master/src/model/ReservaEquipamento.java.
+ */
 
 package model;
 
@@ -10,32 +10,42 @@ import exception.ReservaException;
 
 public class ReservaEquipamento extends Reserva {
 
-    private Equipamento equipamento;
+	private Equipamento equipamento;
 
-    // Mensages
-    private final String EQUIPAMENTO_NULO = "O equipamneto esta nulo.";
+	// Mensages
+	private final String EQUIPAMENTO_NULO = "O equipamneto esta nulo.";
 
-    public ReservaEquipamento(String data, String hora, Equipamento equipamento) throws ReservaException {
-        super(data, hora);
-        this.setEquipamento(equipamento);
-    }
+	public ReservaEquipamento (String data, String hora, Equipamento equipamento)
+			throws ReservaException {
 
-    public Equipamento getEquipamento() {
-        return this.equipamento;
-    }
+		super(data, hora);
+		this.setEquipamento(equipamento);
+	}
 
-    public void setEquipamento(Equipamento equipamento) throws ReservaException {
-        if (equipamento == null)
-            throw new ReservaException(EQUIPAMENTO_NULO);
-        this.equipamento = equipamento;
-    }
+	public Equipamento getEquipamento ( ) {
 
-    public boolean equals(ReservaEquipamento obj) {
-        return (super.equals(obj) && this.getEquipamento().equals(obj.getEquipamento()));
-    }
+		return this.equipamento;
+	}
 
-    @Override public String toString() {
-        return "ReservaEquipamento [equipamento=" + this.getEquipamento() + ", toString()=" + super.toString() + "]";
-    }
+	public void setEquipamento (Equipamento equipamento)
+			throws ReservaException {
+
+		if (equipamento == null)
+			throw new ReservaException(EQUIPAMENTO_NULO);
+		this.equipamento = equipamento;
+	}
+
+	public boolean equals (ReservaEquipamento obj) {
+
+		return (super.equals(obj) && this.getEquipamento().equals(
+				obj.getEquipamento()));
+	}
+
+	@Override
+	public String toString ( ) {
+
+		return "ReservaEquipamento [equipamento=" + this.getEquipamento()
+				+ ", toString()=" + super.toString() + "]";
+	}
 
 }
--- src/model/Sala.java
+++ src/model/Sala.java
@@ -1,8 +1,7 @@
 /**
-		Sala.
-		Class sets exceptions of Sala.
-		https://github.com/ParleyMartins/Tecnicas/tree/master/src/model/Sala.java.
-*/
+ * Sala. Class sets exceptions of Sala.
+ * https://github.com/ParleyMartins/Tecnicas/tree/master/src/model/Sala.java.
+ */
 
 package model;
 
@@ -11,44 +10,50 @@ import exception.PatrimonioException;
 public class Sala extends Patrimonio {
 
 	private String capacidade;
-	
 
-	//Alerts and error mensages
-		private final String CAPACIDADE_INVALIDO = "Capacidade Invalida.";
-		private final String CAPACIDADE_BRANCO = "Capacidade em Branco.";
-		private final String CAPACIDADE_NULA = "Capacidade esta nula.";
-		//private final String CAPACIDADE_NEGATIVA = "Capacidade negativa.";
-			
-		
-	public Sala(String codigo, String descricao, String capacidade) throws PatrimonioException {
+	// Alerts and error mensages
+	private final String CAPACIDADE_INVALIDO = "Capacidade Invalida.";
+	private final String CAPACIDADE_BRANCO = "Capacidade em Branco.";
+	private final String CAPACIDADE_NULA = "Capacidade esta nula.";
+
+	// private final String CAPACIDADE_NEGATIVA = "Capacidade negativa.";
+
+	public Sala (String codigo, String descricao, String capacidade)
+			throws PatrimonioException {
+
 		super(codigo, descricao);
 		this.setCapacidade(capacidade);
 	}
 
-	public String getCapacidade() {
+	public String getCapacidade ( ) {
+
 		return capacidade;
 	}
 
-	public void setCapacidade(String capacidade) throws PatrimonioException {
-		if(capacidade == null)
+	public void setCapacidade (String capacidade) throws PatrimonioException {
+
+		if (capacidade == null)
 			throw new PatrimonioException(CAPACIDADE_NULA);
-		else if("".equals(capacidade.trim()))
-			throw new PatrimonioException(CAPACIDADE_BRANCO);
-		else if(capacidade.matches("[\\d]+")){
-				this.capacidade = capacidade;
-		}
 		else
-		{
-			throw new PatrimonioException(CAPACIDADE_INVALIDO);
-		}
+			if ("".equals(capacidade.trim()))
+				throw new PatrimonioException(CAPACIDADE_BRANCO);
+			else
+				if (capacidade.matches("[\\d]+")) {
+					this.capacidade = capacidade;
+				}
+				else
+				{
+					throw new PatrimonioException(CAPACIDADE_INVALIDO);
+				}
 	}
 
-	public boolean equals(Sala b){
-		if( super.equals(b) &&
-			this.getCapacidade().equals(b.getCapacidade())){
+	public boolean equals (Sala b) {
+
+		if (super.equals(b) &&
+				this.getCapacidade().equals(b.getCapacidade())) {
 			return true;
 		}
-		
+
 		return false;
 	}
 }
