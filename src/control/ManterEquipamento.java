52d81c0bfbafd0fa0ed287706770624d374ead90
Vanusa <vanusa_marcelino@yahoo.com.br>
2014-03-27 15:50:11 -0300

    Changed the comments of class ManterEquipamento.java

--- src/control/ManterEquipamento.java
+++ src/control/ManterEquipamento.java
@@ -1,6 +1,6 @@
 /**
 ManterEquipamento
-Include code and description of equipment, change and delete equipment.
+This class receives equipments data and give them to persistence classes.
 https://github.com/ParleyMartins/Tecnicas/blob/estiloDesign/src/control/ManterEquipamento.java
 */
 package control;
@@ -13,9 +13,7 @@ import model.Equipamento;
 
 public class ManterEquipamento {
 
-	private Vector <Equipamento> Equipamento_vet = new Vector <Equipamento>();
-
-	// Singleton implementation.
+	private Vector <Equipamento> Equipamento_vet = new Vector <Equipamento>( );
 
 	private static ManterEquipamento instance;
 
@@ -24,54 +22,55 @@ public class ManterEquipamento {
 		// Blank constructor.
 	}
 
+	// This constructor provides the singleton implementation.
 	public static ManterEquipamento getInstance ( ) {
 
 		if (instance == null) {
-			instance = new ManterEquipamento();
+			instance = new ManterEquipamento( );
 		}
 		return instance;
 	}
 
-
+	// This method gets a equipment vector.
 	public Vector <Equipamento> getEquipamento_vet ( ) throws SQLException,
 			PatrimonioException {
 
-		this.Equipamento_vet = EquipamentoDAO.getInstance().searchAll();
+		this.Equipamento_vet = EquipamentoDAO.getInstance( ).searchAll( );
 		return this.Equipamento_vet;
 	}
 
-	// include code and description of the equipment in the database.
+	// This method include code and description of the equipment in the database.
 	public void inserir (String codigo, String descricao)
 			throws PatrimonioException, SQLException {
 
 		Equipamento equipamento = new Equipamento(codigo, descricao);
-		EquipamentoDAO.getInstance().include(equipamento);
-		getEquipamento_vet();
+		EquipamentoDAO.getInstance( ).include(equipamento);
+		getEquipamento_vet( );
 	}
 
-	// Update code and description info in the database.
+	// This method update code and description info in the database.
 	public void alterar (String codigo, String descricao,
 			Equipamento equipamento) throws PatrimonioException, SQLException {
 
 		if (equipamento == null) {
 			throw new PatrimonioException("Equipamento em branco");
 		}
-		Equipamento old_equipamento = new Equipamento(equipamento.getCodigo(),
-				equipamento.getDescricao());
+		Equipamento old_equipamento = new Equipamento(equipamento.getCodigo( ),
+				equipamento.getDescricao( ));
 		equipamento.setCodigo(codigo);
 		equipamento.setDescricao(descricao);
-		EquipamentoDAO.getInstance().change(old_equipamento, equipamento);
-		getEquipamento_vet();
+		EquipamentoDAO.getInstance( ).change(old_equipamento, equipamento);
+		getEquipamento_vet( );
 	}
 
-	//Remove equipment form the database.
+	//This method deletes the selected equipment.
 	public void excluir (Equipamento equipamento) throws SQLException,
 			PatrimonioException {
 
 		if (equipamento == null) {
 			throw new PatrimonioException("Equipamento em branco");
 		}
-		EquipamentoDAO.getInstance().exclude(equipamento);
-		getEquipamento_vet();
+		EquipamentoDAO.getInstance( ).exclude(equipamento);
+		getEquipamento_vet( );
 	}
 }
