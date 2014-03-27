a65a874cced0f9c15f185a72c0d5ada34d89321a
Vanusa <vanusa_marcelino@yahoo.com.br>
2014-03-27 13:24:30 -0300

    Indentation of the class ManterResSalaProfessor

--- src/control/ManterResSalaProfessor.java
+++ src/control/ManterResSalaProfessor.java
@@ -13,50 +13,66 @@ import exception.PatrimonioException;
 import exception.ReservaException;
 
 public class ManterResSalaProfessor {
-	private Vector<ReservaSalaProfessor> rev_sala_professor_vet = new Vector<ReservaSalaProfessor>();
-	
-	//Singleton
-		private static ManterResSalaProfessor instance;
-		private ManterResSalaProfessor() {
-		}
-		public static ManterResSalaProfessor getInstance() {
-		if(instance == null)
+
+	private Vector <ReservaSalaProfessor> rev_sala_professor_vet = new Vector <ReservaSalaProfessor>();
+
+	// Singleton
+	private static ManterResSalaProfessor instance;
+
+	private ManterResSalaProfessor ( ) {
+
+	}
+
+	public static ManterResSalaProfessor getInstance ( ) {
+
+		if (instance == null)
 			instance = new ManterResSalaProfessor();
 		return instance;
 	}
+
 	//
-		
-		public Vector<ReservaSalaProfessor> buscarPorData(String data) throws SQLException, ClienteException, PatrimonioException, ReservaException{
-	        return ResSalaProfessorDAO.getInstance().buscarPorData(data);
-	    } 
-	    	
-		
-	public Vector<ReservaSalaProfessor> getResProfessorSala_vet() throws SQLException, ClienteException, PatrimonioException, ReservaException {
-		this.rev_sala_professor_vet = ResSalaProfessorDAO.getInstance().buscarTodos();
+
+	public Vector <ReservaSalaProfessor> buscarPorData (String data)
+			throws SQLException, ClienteException, PatrimonioException,
+			ReservaException {
+
+		return ResSalaProfessorDAO.getInstance().buscarPorData(data);
+	}
+
+	public Vector <ReservaSalaProfessor> getResProfessorSala_vet ( )
+			throws SQLException, ClienteException, PatrimonioException,
+			ReservaException {
+
+		this.rev_sala_professor_vet = ResSalaProfessorDAO.getInstance()
+				.buscarTodos();
 		return this.rev_sala_professor_vet;
 	}
 
-	public void inserir(Sala sala, Professor prof,
-						String data, String hora, String finalidade) 
-					throws SQLException, ReservaException {
+	public void inserir (Sala sala, Professor prof,
+			String data, String hora, String finalidade)
+			throws SQLException, ReservaException {
 
-		ReservaSalaProfessor reserva = new ReservaSalaProfessor(data, hora, sala , finalidade, prof);
+		ReservaSalaProfessor reserva = new ReservaSalaProfessor(data, hora,
+				sala, finalidade, prof);
 		ResSalaProfessorDAO.getInstance().incluir(reserva);
 		this.rev_sala_professor_vet.add(reserva);
 	}
 
-	public void alterar(String finalidade, ReservaSalaProfessor reserva) 
-				throws SQLException, ReservaException {
-		
-		ReservaSalaProfessor reserva_old = new ReservaSalaProfessor(reserva.getData(), reserva.getHora(), reserva.getSala() , 
+	public void alterar (String finalidade, ReservaSalaProfessor reserva)
+			throws SQLException, ReservaException {
+
+		ReservaSalaProfessor reserva_old = new ReservaSalaProfessor(
+				reserva.getData(), reserva.getHora(), reserva.getSala(),
 				reserva.getFinalidade(), reserva.getProfessor());
-		
+
 		reserva.setFinalidade(finalidade);
 		ResSalaProfessorDAO.getInstance().alterar(reserva_old, reserva);
-		
+
 	}
 
-	public void excluir(ReservaSalaProfessor reserva) throws SQLException, ReservaException {
+	public void excluir (ReservaSalaProfessor reserva) throws SQLException,
+			ReservaException {
+
 		ResSalaProfessorDAO.getInstance().excluir(reserva);
 		this.rev_sala_professor_vet.remove(reserva);
 	}
