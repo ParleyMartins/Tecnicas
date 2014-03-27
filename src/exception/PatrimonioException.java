f64b0c40e390fad54d1e0bab6fa0180fcbea393c
Vanusa <vanusa_marcelino@yahoo.com.br>
2014-03-27 18:56:13 -0300

    creating in class comment PatrimonioException

--- src/exception/PatrimonioException.java
+++ src/exception/PatrimonioException.java
@@ -1,12 +1,18 @@
+/**
+  PatrimonioException
+  https://github.com/ParleyMartins/Tecnicas/blob/estiloDesign/src
+  /exception/PatrimonioException.java
+ */
 package exception;
 
 @SuppressWarnings("serial")
 public class PatrimonioException extends Exception {
 
+	// Constructor creates a PatrimonioExcpetion without any argument.
 	public PatrimonioException() {
 		super();
 	}
-
+	// Constructor creates a PatrimonioExcpetion with a message.
 	public PatrimonioException(String msg) {
 		super(msg);
 	}
