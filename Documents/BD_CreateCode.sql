0cab68de28565b4de1bce7a36a4dd3bc2b9507fb
ParleyMartins <parleypachecomartins@gmail.com>
2014-04-25 17:48:42 -0300

    Added tutorial and README updated.

---
+++ Documents/BD_CreateCode.sql
@@ -0,0 +1,90 @@
+DROP DATABASE IF EXISTS sisres_db;
+CREATE DATABASE sisres_db;
+CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'password';
+USE sisres_db;
+GRANT ALL ON sisres_db.* TO 'testuser'@'localhost';
+
+CREATE TABLE aluno (
+ id_aluno INT NOT NULL AUTO_INCREMENT,
+ nome VARCHAR(100) NOT NULL,
+ cpf VARCHAR(14) NOT NULL,
+ telefone VARCHAR(15),
+ email VARCHAR(60),
+ matricula VARCHAR(15) NOT NULL,
+ PRIMARY KEY (id_aluno)
+);
+
+
+CREATE TABLE equipamento (
+ id_equipamento INT NOT NULL AUTO_INCREMENT,
+ codigo VARCHAR(15) NOT NULL,
+ descricao VARCHAR(120),
+ PRIMARY KEY (id_equipamento)
+);
+
+
+CREATE TABLE professor (
+ id_professor INT NOT NULL AUTO_INCREMENT,
+ nome VARCHAR(100) NOT NULL,
+ cpf VARCHAR(14) NOT NULL,
+ telefone VARCHAR(15),
+ email VARCHAR(60),
+ matricula VARCHAR(15) NOT NULL,
+ PRIMARY KEY (id_professor)
+);
+
+
+CREATE TABLE reserva_equipamento_professor (
+ id_reserva_equipamento_professor INT NOT NULL AUTO_INCREMENT,
+ id_professor INT NOT NULL,
+ id_equipamento INT NOT NULL,
+ hora VARCHAR(5) NOT NULL,
+ data VARCHAR(10) NOT NULL,
+ PRIMARY KEY (id_reserva_equipamento_professor)
+);
+
+
+CREATE TABLE sala (
+ id_sala INT NOT NULL AUTO_INCREMENT,
+ codigo VARCHAR(10) NOT NULL,
+ descricao VARCHAR(120),
+ capacidade INT,
+ PRIMARY KEY (id_sala)
+);
+
+
+CREATE TABLE reserva_sala_aluno (
+ id_reserva_sala_aluno INT NOT NULL AUTO_INCREMENT,
+ id_aluno INT NOT NULL,
+ id_sala INT NOT NULL,
+ finalidade VARCHAR(150) NOT NULL,
+ hora VARCHAR(5) NOT NULL,
+ data VARCHAR(10) NOT NULL,
+ cadeiras_reservadas INT NOT NULL,
+ PRIMARY KEY (id_reserva_sala_aluno)
+);
+
+
+CREATE TABLE reserva_sala_professor (
+ id_reserva_sala_professor INT NOT NULL AUTO_INCREMENT,
+ id_professor INT NOT NULL,
+ id_sala INT NOT NULL,
+ finalidade VARCHAR(150) NOT NULL,
+ hora VARCHAR(5) NOT NULL,
+ data VARCHAR(10) NOT NULL,
+ PRIMARY KEY (id_reserva_sala_professor)
+);
+
+
+ALTER TABLE reserva_equipamento_professor ADD CONSTRAINT FK_reserva_equipamento_professor_0 FOREIGN KEY (id_professor) REFERENCES professor (id_professor);
+ALTER TABLE reserva_equipamento_professor ADD CONSTRAINT FK_reserva_equipamento_professor_1 FOREIGN KEY (id_equipamento) REFERENCES equipamento (id_equipamento);
+
+
+ALTER TABLE reserva_sala_aluno ADD CONSTRAINT FK_reserva_sala_aluno_0 FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno);
+ALTER TABLE reserva_sala_aluno ADD CONSTRAINT FK_reserva_sala_aluno_1 FOREIGN KEY (id_sala) REFERENCES sala (id_sala);
+
+
+ALTER TABLE reserva_sala_professor ADD CONSTRAINT FK_reserva_sala_professor_0 FOREIGN KEY (id_professor) REFERENCES professor (id_professor);
+ALTER TABLE reserva_sala_professor ADD CONSTRAINT FK_reserva_sala_professor_1 FOREIGN KEY (id_sala) REFERENCES sala (id_sala);
+
+
--- README.txt
+++ README.txt
@@ -2,11 +2,10 @@ Tecnicas
 ========
 
 So the program can be corrected compiled, the following steps must be taken (only before the first compilation):
-	1. Add the libs: jcalendar-1.4.jar and mysql-connector-java-5.1.22-bin.jar to the Project Libraries (click with the right button on JRE System Library > Build Path > Configure Build Path > Add External Jars > (folder libs from the repo)
+	1. Add the libs: jcalendar-1.4.jar and mysql-connector-java-5.1.22-bin.jar to the Project Libraries (click with the right button on JRE System Library > Build Path > Configure Build Path > Add External Jars > (folder "libs" from the repo)
 	2. Install mysql
-	3. Type mysql -u 'testuser' -p
-	4. Type the password: password
-	6. Execute the file BD_CreateCode.sql (in documents folder) on terminal (you can copy and paste it on terminal)
-	7. Compile the program and enjoy it.
+	3. Log in with your root account (You are supposed to had set that up when you installed mysql)
+	4. Execute the file BD_CreateCode.sql (in documents folder) on terminal (you can copy and paste it on terminal)
+	5. Compile the program and enjoy it.
 
 
