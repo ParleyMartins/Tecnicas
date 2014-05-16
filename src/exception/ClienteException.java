dc485ca944f099e40a24c17ade0482b390cfe399
Thiago Meira Bernardes <thiagobernardes10@gmail.com>
2014-05-16 10:06:13 -0300

    Modified comments of class for the default acepted.

--- src/exception/ClienteException.java
+++ src/exception/ClienteException.java
@@ -1,6 +1,6 @@
 /**
 ClienteException
-Exception type to Cliente's errors
+Client Exceptions include constructors to create a exceptions for the erros related to client.
 https://github.com/ParleyMartins/Tecnicas/blob/estiloDesign/src/exception/ClienteException.java
 */
 
