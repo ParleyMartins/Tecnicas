f33b9adbfe40b5cffae6ff66f6702c1d70b0b8bb
Vanusa <vanusa_marcelino@yahoo.com.br>
2014-05-23 10:45:19 -0300

    Adding significant comment in class ReservaEquioamento

--- src/model/ReservaEquipamento.java
+++ src/model/ReservaEquipamento.java
@@ -14,8 +14,16 @@ public class ReservaEquipamento extends Reserva {
 	private Equipamento equipment;
 
 	// Error messages.
-	private final String BLANK_EQUIPMENT = International.getInstance().getMessages().getString("blankEquipment");
+	private final String BLANK_EQUIPMENT = International.getInstance().getMessages()
+			.getString("blankEquipment");
 
+	/**
+	 * This method is for equipment reservation.
+	 * @param date reservation date equipment.
+	 * @param time reservation time equipment.
+	 * @param equipment object to be reservation.
+	 * @throws ReservaException It ensures that every parameter passed is valid. 
+	 */
 	public ReservaEquipamento(String date, String time, Equipamento equipment)
 			throws ReservaException {
 
@@ -23,11 +31,20 @@ public class ReservaEquipamento extends Reserva {
 		this.setEquipment(equipment);
 	}
 
+	/**
+	 * This method gets a equipment. 
+	 * @return The content in the equipment field.
+	 */
 	public Equipamento getEquipment() {
 
 		return this.equipment;
 	}
 
+	/**
+	 * This method modifies the equipment field.
+	 * @param equipment object to be reservation.
+	 * @throws ReservaException It ensures that every parameter passed is valid.
+	 */
 	public void setEquipment(Equipamento equipment) throws ReservaException {
 
 		if (equipment == null) {
@@ -38,13 +55,20 @@ public class ReservaEquipamento extends Reserva {
 		this.equipment = equipment;
 	}
 
+	/**
+	 * This method checks a equipment.
+	 * @param reservation A reservation.
+	 * @return true if there is a reservation. false otherwise.
+	 */
 	public boolean equals(ReservaEquipamento reservation) {
 
 		return (super.equals(reservation) && this.getEquipment()
 				.equals(reservation.getEquipment()));
 	}
 
-	@Override
+	/**This method returns a String object representing the data.
+	 * @return A reservation.
+	 */
 	public String toString() {
 
 		return "ReservaEquipamento [equipamento=" + this.getEquipment()
