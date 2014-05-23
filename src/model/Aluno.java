5d743fe721414596db6793c8b27ec8e2d293189d
Vanusa <vanusa_marcelino@yahoo.com.br>
2014-05-23 10:36:41 -0300

    adding significant comment in class Aluno

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -12,14 +12,28 @@ import exception.ClienteException;
 public class Aluno extends Cliente {
 
 	// Error messages and alerts.
-	private final String BLANK_ENROLLMENT_NUMBER = International.getInstance().getMessages().getString("blankEnrollment");
+	private final String BLANK_ENROLLMENT_NUMBER = International.getInstance().getMessages()
+					.getString("blankEnrollment");
 
+	/**
+	 * Basic attributes of registration.
+	 * @param name is how the student is called.
+	 * @param cpf is the code that every citizen has.
+	 * @param enrollment_number is the number of enrolled student.
+	 * @param phone_number is the phone number of the student.
+	 * @param email is the electronic address of the student.
+	 * @throws ClienteExceptionprevention error when calling the method.
+	 */
 	public Aluno(String name, String cpf, String enrollment_number,
 			String phone_number, String email) throws ClienteException {
 
 		super(name, cpf, enrollment_number, phone_number, email);
 	}
 
+	/** set the registration.
+	 * @param enrrollment_number is the number of enrolled student.
+	 * @throws ClienteException prevention error when calling the method.
+	 */
 	public void setEnrollmentNumber(String enrollment_number)
 			throws ClienteException {
 
