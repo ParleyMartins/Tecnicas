e1484299b1a2a732ba157708ef5b84d3504bd26f
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-06-01 20:58:18 -0300

    Added search tests to ManterProfessorTest

--- src/test/control/ManterProfessorTest.java
+++ src/test/control/ManterProfessorTest.java
@@ -141,6 +141,116 @@ public class ManterProfessorTest {
 		ManterProfessor.getInstance().delete(teacher);
 	}
 
+	@Test
+	public void searchByName() throws ClienteException, SQLException {
+		Professor teacher = new Professor("Incluindo", "040.757.021-70", "123456",
+				"9999-9999", "aluno@email");
+		Vector<Professor> teachers;
+
+		insert(teacher);
+
+		teachers = ManterProfessor.getInstance().searchName(teacher.getName());
+		assertEquals(1, teachers.size());
+		
+		delete(teacher);
+	}
+	
+	@Test
+	public void searchByNameInexistent() throws ClienteException, SQLException {
+		Vector<Professor> teachers;
+		
+		teachers = ManterProfessor.getInstance().searchName("Nome do professor");
+		assertEquals(0, teachers.size());
+	}
+	
+	@Test
+	public void searchByCPF() throws ClienteException, SQLException {
+		Professor teacher = new Professor("Incluindo", "040.757.021-70", "123456",
+				"9999-9999", "aluno@email");
+		Vector<Professor> teachers;
+
+		insert(teacher);
+
+		teachers = ManterProfessor.getInstance().searchCpf(teacher.getCpf());
+		assertEquals(1, teachers.size());
+		
+		delete(teacher);
+	}
+	
+	@Test
+	public void searchByCPFInexistent() throws ClienteException, SQLException {
+		Vector<Professor> teachers;
+		
+		teachers = ManterProfessor.getInstance().searchCpf("040.757.021-70");
+		assertEquals(0, teachers.size());
+	}
+	
+	@Test
+	public void searchByEnrollmentNumber() throws ClienteException, SQLException {
+		Professor teacher = new Professor("Incluindo", "040.757.021-70", "123456",
+				"9999-9999", "aluno@email");
+		Vector<Professor> teachers;
+
+		insert(teacher);
+
+		teachers = ManterProfessor.getInstance().searchEnrollNumber(teacher.getEnrollmentNumber());
+		assertEquals(1, teachers.size());
+		
+		delete(teacher);
+	}
+	
+	@Test
+	public void searchByEnrollmentNumberInexistent() throws ClienteException, SQLException {
+		Vector<Professor> teachers;
+		
+		teachers = ManterProfessor.getInstance().searchEnrollNumber("1111");
+		assertEquals(0, teachers.size());
+	}
+
+	@Test
+	public void searchByEmail() throws ClienteException, SQLException {
+		Professor teacher = new Professor("Incluindo", "040.757.021-70", "123456",
+				"9999-9999", "aluno@email");
+		Vector<Professor> teachers;
+
+		insert(teacher);
+
+		teachers = ManterProfessor.getInstance().searchEmail(teacher.getEmail());
+		assertEquals(1, teachers.size());
+		
+		delete(teacher);
+	}
+	
+	@Test
+	public void searchByEmailInexistent() throws ClienteException, SQLException {
+		Vector<Professor> teachers;
+		
+		teachers = ManterProfessor.getInstance().searchEmail("teacher@email");
+		assertEquals(0, teachers.size());
+	}
+	
+	@Test
+	public void searchByPhoneNumber() throws ClienteException, SQLException {
+		Professor teacher = new Professor("Incluindo", "040.757.021-70", "123456",
+				"9999-9999", "aluno@email");
+		Vector<Professor> teachers;
+
+		insert(teacher);
+
+		teachers = ManterProfessor.getInstance().searchPhoneNumber(teacher.getPhoneNumber());
+		assertEquals(1, teachers.size());
+		
+		delete(teacher);
+	}
+	
+	@Test
+	public void searchByPhoneNumberInexistent() throws ClienteException, SQLException {
+		Vector<Professor> teachers;
+		
+		teachers = ManterProfessor.getInstance().searchEmail("9999-9999");
+		assertEquals(0, teachers.size());
+	}
+	
 	private void insert(Professor teacher) throws SQLException {
 
 		this.executaNoBanco("INSERT INTO "
